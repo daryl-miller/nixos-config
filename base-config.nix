d3427778b94463074ab12f003aeda4aa6abd0c53
dmiller <dev@darylmiller.me>
2023-10-16 15:46:28 +1100

    Enable autologin because I'm rebooting every 5 minutes while I test out packages

--- base-config.nix
+++ base-config.nix
@@ -50,6 +50,8 @@
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.sddm.autoLogin.enable = true;
+  services.xserver.displayManager.sddm.autoLogin.user = "dmiller";
 
   # Configure keymap in X11
   services.xserver = {
