e1545ab32f506aafadd28cd2f1f7730c179e4c9e
dmiller <dev@darylmiller.me>
2023-10-16 18:41:59 +1100

    Get league working in bottles

--- base-config.nix
+++ base-config.nix
@@ -106,8 +106,14 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    btop
+    bottles
+    wineWowPackages.stable
+    wineWowPackages.waylandFull
+    # League fonts
+    corefonts
+    vulkan-tools
+    lutris
   ];
 
   programs.steam = {
